60822c30d2343887a5cfa676464ed23734201b41
Ahmed Shams <96126679+0xSh3RlocK@users.noreply.github.com>
2022-08-12 04:47:53 +0200

    Add files via upload

---
+++ server/server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            IPHostEntry ipe = Dns.GetHostEntry("localhost");
+            IPAddress[] ips = ipe.AddressList;
+
+            foreach (IPAddress ip in ips)
+            {
+                Console.WriteLine(ip.ToString());
+
+            }
+            Console.ReadKey();
+            */
+           // Console.WriteLine(IPAddress.Any);
+
+            Socket ss = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            ss.Bind(new IPEndPoint(IPAddress.Any, 1234));
+            ss.Listen(5);
+
+            Socket cs = ss.Accept();
+            Console.WriteLine("[+] Connection From: {0}", cs.RemoteEndPoint);
+
+            Console.WriteLine("Enter A message To send: ");
+            string msg;
+            msg = Console.ReadLine();
+            cs.Send(Encoding.ASCII.GetBytes(msg));
+            while (msg != "quit")
+            {
+               
+                byte[] buf = new byte[2048];
+                Array.Clear(buf, 0, buf.Length);
+
+                cs.Receive(buf);
+
+                Console.WriteLine(Encoding.ASCII.GetString(buf).TrimEnd('\0'));
+
+                Console.WriteLine("Enter A message To send: ");
+                msg = Console.ReadLine();
+                cs.Send(Encoding.ASCII.GetBytes(msg));
+            }
+            cs.Close();
+            ss.Close();
+             
+
+            //Console.ReadKey();
+
+        }
+    }
+}
